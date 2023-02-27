a9f2a4b856afee573df45a899513fc2d45f09819
forestlet <hyf2021@outlook.com>
2023-02-27 17:49:09 +0800

    🐞 fix: fix calendar lang

--- src/components/Collection/Filter.vue
+++ src/components/Collection/Filter.vue
@@ -26,7 +26,7 @@ import { read_tags } from '@/util/tag';
 import { filter_collection, collection_filtered } from "@/util/piece"
 import { select_tag, date_range } from '@/data/filter';
 import { gap } from '@/data/style';
-import { i18n } from '@/i18n/i18n';
+import { shortcuts } from '@/data/locale';
 
 const tags = read_tags()
 const update_collection = () => {
@@ -36,71 +36,6 @@ const update_collection = () => {
 const toggle_gap = () => {
   gap.value = !gap.value
 }
-
-let shortcuts = [
-  {
-    text: i18n.global.t("message.filter.shortcuts.today"),
-    value: () => {
-      const end = new Date()
-      const start = new Date()
-      return [start, end]
-    },
-  },
-  {
-    text: i18n.global.t("message.filter.shortcuts.this-week"),
-    value: () => {
-      const end = new Date()
-      const start = new Date()
-      start.setTime(start.getTime() - (start.getDay() - 1) * 24 * 60 * 60 * 1000)
-      return [start, end]
-    },
-  },
-  {
-    text: i18n.global.t("message.filter.shortcuts.this-month"),
-    value: () => {
-      const end = new Date()
-      const start = new Date()
-      start.setTime(start.setDate(1))
-      return [start, end]
-    },
-  },
-  {
-    text: i18n.global.t("message.filter.shortcuts.this-year"),
-    value: () => {
-      const end = new Date()
-      const start = new Date()
-      start.setTime(new Date(start.setMonth(0)).setDate(1))
-      return [start, end]
-    },
-  },
-  {
-    text: i18n.global.t("message.filter.shortcuts.last-week"),
-    value: () => {
-      const end = new Date()
-      const start = new Date()
-      start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
-      return [start, end]
-    },
-  },
-  {
-    text: i18n.global.t("message.filter.shortcuts.last-month"),
-    value: () => {
-      const end = new Date()
-      const start = new Date()
-      start.setTime(start.setMonth(start.getMonth() - 1))
-      return [start, end]
-    },
-  },
-  {
-    text: i18n.global.t("message.filter.shortcuts.last-year"),
-    value: () => {
-      const end = new Date()
-      const start = new Date()
-      start.setTime(start.setFullYear(start.getFullYear() - 1))
-      return [start, end]
-    },
-  },
-]
 </script>
 
 <style scoped lang="scss">
